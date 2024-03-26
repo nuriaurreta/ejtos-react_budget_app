6f2b8967fbde35b94abae5a4d7c50c8fbb0e6616
nuriaurreta <nurreta@gmail.com>
2024-03-26 14:23:44 -0400

    modified budget.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -58,16 +58,17 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                  <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                <label className="input-group-text" >£</label>
+                  </div>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{ size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
-
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
                     </button>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,14 +4,34 @@ import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const [errorMessage, setErrorMessage] = useState('');
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
+        const newBudgetValue = event.target.value;
+        if (parseInt(newBudgetValue) > 20000) {
+            setErrorMessage('The budget cannot exceed £20,000.');
+        } else {
+            setErrorMessage('');
+            setNewBudget(newBudgetValue);
+        }
+    };
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: £ </span>
+            <input
+                type="number"
+                step="10"
+                value={newBudget}
+                onChange={handleBudgetChange}
+            />
+            {errorMessage && (
+                <div className="alert alert-danger mt-2" role="alert">
+                    {errorMessage}
+                </div>
+            )}
+        </div>
     );
 };
+
 export default Budget;
